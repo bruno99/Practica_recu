ceff99cc6fb80c3e5468782cae1dd06eea4f4e3d
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-01 17:55:14 -0100

    Update Query.ts

--- resolvers/Query.ts
+++ resolvers/Query.ts
@@ -1,8 +1,25 @@
 Import {Collection, Database} from "https://deno.land/x/mongo@v0.13.0/mod.ts";
 import { IContext, IUser } from "../types.ts"
-
+import { GQLError } from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
  
 export const Query = {
+ 
+  getCar: async(parent: any, args; aby, ctx: IContext): Promise<CarSchema[]> => {
+       const db: Database = ctx.db;
+      const carsCollection = db.collection<CarSchema>("Cars");
+      const cars = await coarsCollection.find();
+      const result = cars.map((t) => {
+        return {
+          ...t,
+          plate: t.plate.toString(),
+        };
+      });
+      return result;
+    } catch (e) {
+      throw new GQLError(e);
+   },
+ 
+ 
    getTrips: async (parent: any, args: any, ctx: IContext): Promise<TripSchema[]> => {
       const {user, db} = {...ctx};
       
@@ -17,7 +34,41 @@ export const Query = {
       const CarsCollection:Collection<CarSchema> = db.collection<CarSchema>("Cars");
       
       return await CarsCollection.find();
+   },
+ getDrivers: async(parent: any, args: any, ctx: IContext); Promise<UserSchema[]> => {
+     const db = ctx.db;
+ 
+     const UserCollection: Collection>UserSchema> = db.collection<UserSchema>("Users");
+ 
+     return await UserCollection.find({ role: Role:DRIVER });
+ },
+  getClients: async(parent: any, args: any, ctx: IContext); Promise<UserSchema[]> => {
+     const db = ctx.db;
+ 
+     const UserCollection: Collection>UserSchema> = db.collection<UserSchema>("Users");
+ 
+     return await UserCollection.find({ role: Role:CLIENT });
+ },
+   getStatus: async ( parent: any, args: IGetCarArgs, ctx: IContext): Promise<CarSchema>[] | null> => {
+ 
+      const db: Database = ctx.db;
+      const cars = db.collection<CarSchema>("Cars");
+      const car: CarSchema | null = await cars.findOne({ available: args.available });
+
+      let available: string;
+      if (car) {
+        available = car.available.toString();
+        return {...car, available};
+      }
+
+      return null;
+    catch (e) {
+      throw new GQLError(e);
+    }
+  },
+ 
       
    
-   
-}
+};
+
+export {Query}

f1039cad403c8391a884ebbad0e51137d47e6075
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-01 16:26:55 -0100

    Create schema.ts

---
+++ mongo/schema.ts
@@ -0,0 +1,25 @@
+
+export enum Role {
+DRIVER,
+CLIENT,
+ADMIN,
+}
+export interface ClientSchema{
+     email: string;
+     role: Role,
+     password: string,
+     token: string,
+     
+}
+export interface CarSchema{
+    plate: string;
+    driver: string;
+    available: boolean;
+}
+
+export interface TripSchema{
+    _id: { $oid: string },
+    driver: string,
+    client: string;
+    car: string;
+}

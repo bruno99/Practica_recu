02a65c7c494d7283a5074288e548da97e5570d24
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-01 17:41:07 -0100

    Update Car.ts

--- resolvers/Car.ts
+++ resolvers/Car.ts
@@ -8,7 +8,7 @@ const Car = {
     driver: async(parent: { driver: string }, args: any, ctx: IContext):Promise<UserSchema> => {
         const db = ctx.db;
         const UsersCollection: Collection<UserSchema> = db.collection>UserSchema>("Users");
-        const driver = await UserCollection.finOne({email: parent.driver, role: Role.DRIVER
+        const driver = await UserCollection.finOne({email: parent.driver, role: Role.DRIVER});
         
         if(driver) return driver;
         throw new GQLError("Driver not found");

1bb70e110351ad32b55f88bfaa8c667a6f2e60e0
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-01 17:28:43 -0100

    Create Car.ts

---
+++ resolvers/Car.ts
@@ -0,0 +1,17 @@
+import { Collection } from  "https://deno.land/x/mongo@v0.13.0/ts/collection.ts";
+import { GQLError } from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
+import { CarSchema, Role, UserSchema } from "../mongo/schema.ts";
+import { IContext } from "../types.ts";
+
+
+
+const Car = {
+    driver: async(parent: { driver: string }, args: any, ctx: IContext):Promise<UserSchema> => {
+        const db = ctx.db;
+        const UsersCollection: Collection<UserSchema> = db.collection>UserSchema>("Users");
+        const driver = await UserCollection.finOne({email: parent.driver, role: Role.DRIVER
+        
+        if(driver) return driver;
+        throw new GQLError("Driver not found");
+    }
+}

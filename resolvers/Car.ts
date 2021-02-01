7f504b1db519cbca00c6721bcfb6b1575e0d450a
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-01 17:44:07 -0100

    Update Car.ts

--- resolvers/Car.ts
+++ resolvers/Car.ts
@@ -1,4 +1,5 @@
 import { Collection } from  "https://deno.land/x/mongo@v0.13.0/ts/collection.ts";
+import { GQLError } from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
 import { CarSchema } from "../mongo/schema.ts";
 import { IContext } from "../types.ts";
 
